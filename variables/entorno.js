9b01860ee0a448c2ee33562f0a71b01ba691ea9a
rafaperezgarcia <rafaperezgarcia@gmial.com>
2018-08-06 00:20:39 +0200

    beta 3

---
+++ variables/entorno.js
@@ -0,0 +1,6 @@
+module.exports = {
+  bd: process.env.MONGODB || 'mongodb://rafaperezgarcia:mlabunirbot99@ds211592.mlab.com:11592/unirbot',
+  accessToken: process.env.ACCESSTOKEN || "EAADOT5vJWZBgBAFAHZCszVL7x3fYPgZCUr2F1sT7VyhLZAT1JRlfCtnZAdIircP8DP15QVNiCowqqS9sEMMq7oSzYi65qlGzYouTIrqpapm7BarhxI7DZBdDccC8SCEmHq4x7l1Wi7hrCEbtpDpZBbgdOuTW5906LrtO1sJWSZBnggZDZD",
+  verifyToken: process.env.VERIFYTOKEN || "UNIR",
+  appSecret: process.env.APPSECRET || "7bfa2372feb8fb3d74a86c6af8dcae9c"
+};
